d0d44cb2e33561e40a03ff3b504dd33b1584a132
Geraldine Camino <geraldine.caminodu@amigo.edu.co>
2022-09-27 21:34:56 -0500

    cambios_ult

---
+++ android/app/src/main/java/io/ionic/starter/galeria.java
@@ -0,0 +1,14 @@
+package io.ionic.starter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class galeria extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
